a01d478cdf3d670f3fdda25b6a6228080dbb5859
Konotorii <79553360+konotorii@users.noreply.github.com>
2024-06-19 09:53:52 -0400

    feat: Basic proxy downloader (#4)

---
+++ helpers.go
@@ -0,0 +1,17 @@
+package pecan
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"strings"
+)
+
+func proxyPrivateDownload(token string, url string, res gin.ResponseWriter) {
+	headers := []CustomHeaders{{label: "Accept", value: "application/octet-stream"}}
+
+	strings.Replace(url, "https://api.github.com/", fmt.Sprintf("https://%s@api.github.com/", token), 1)
+
+	getRes := getRequest(url, headers)
+
+	res.Header().Set("Location", getRes.Header.Get("Location"))
+}
