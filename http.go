54cfe12b4d3000460c5dc19fa24d09f183024075
Konotorii <79553360+konotorii@users.noreply.github.com>
2024-06-19 09:53:35 -0400

    feat: Get request helper (#5)

---
+++ http.go
@@ -0,0 +1,42 @@
+package pecan
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type CustomHeaders struct {
+	label string
+	value string
+}
+
+var client = http.Client{}
+
+func getRequest(url string, headers []CustomHeaders) *http.Response {
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		fmt.Printf("client: could not create request: %s\n", err)
+	}
+
+	for _, value := range headers {
+		req.Header.Set(value.label, value.value)
+	}
+
+	res, err := client.Do(req)
+
+	if err != nil {
+		fmt.Printf("error making http request: %s\n", err)
+	}
+
+	fmt.Printf(res.Status)
+
+	return res
+}
+
+func postRequest() {
+
+}
+
+func putRequest() {
+
+}
