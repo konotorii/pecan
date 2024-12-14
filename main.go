7e243c81d6accd67bfefeb264a63a5a1baac5c99
Konotorii <konotoribusiness@gmail.com>
2024-12-14 16:29:36 -0500

    feat: Initiate config in main init (#8)

--- main.go
+++ main.go
@@ -1,32 +1,27 @@
-package pecan
+package main
 
 import (
 	"fmt"
+	"log"
+	"pecan/util"
+
 	"github.com/TwiN/go-color"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
-	"log"
-	"os"
-	"strconv"
 )
-
-var port, port_exists = strconv.Atoi(os.Getenv("PORT"))
-
-var git_token = os.Getenv("GITHUB_TOKEN")
-
-var git_repo = os.Getenv("GITHUB_REPO")
 
 func init() {
 	if err := godotenv.Load(); err != nil {
 		fmt.Println(color.Ize(color.White, "No .env was found!"))
 	}
+
+	util.ConfigInit()
 }
 
 func main() {
-	fmt.Print(port)
 	r := engine()
 	r.Use(gin.Logger())
-	if err := engine().Run(":8080"); err != nil {
+	if err := engine().Run(fmt.Sprintf(":%d", util.Config.Port)); err != nil {
 		log.Fatal("unable to start:", err)
 	}
 }
